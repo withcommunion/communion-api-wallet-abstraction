6840ea64907e4e37acf1bde983b2eb9dbc1a4037
Mike Alicea <mfalicea58@gmail.com>
2022-05-03 14:36:03 -0400

    Adds create-wallet api, unifies naming

---
+++ src/endpoints/api-get-wallet/getWallet.ts
@@ -0,0 +1,18 @@
+import type { APIGatewayProxyHandler } from 'aws-lambda';
+import { generateReturn } from '../api-util';
+// import log from 'lambda-log';
+
+export const handler: APIGatewayProxyHandler = async () => {
+  try {
+    return generateReturn(200, {
+      wallet_key: 'asdf123',
+    });
+  } catch (error) {
+    // log.error('Failed to get wallet', {
+    //   error,
+    // });
+    return generateReturn(500, {
+      message: 'Something went wrong trying to get the wallet',
+    });
+  }
+};
---
+++ src/endpoints/api-util.ts
@@ -0,0 +1,41 @@
+export interface returnObject {
+  statusCode: number;
+  body: string;
+  headers: {
+    'Access-Control-Allow-Origin': '*';
+    'Access-Control-Allow-Credentials': boolean;
+  };
+}
+
+export function generateReturn(
+  statusCode: number,
+  body: Record<string, unknown>
+): returnObject {
+  return {
+    statusCode,
+    body: JSON.stringify(body),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+  };
+}
+
+/*
+export function getMemberFromAuthorizer(event: APIGatewayEvent): {
+  memberId: string;
+  isEmailVerified: boolean;
+} {
+  const isInLocal = process.env.IS_LOCAL === 'true';
+
+  const memberId = isInLocal
+    ? 'dev-id'
+    : event.requestContext.authorizer.claims['cognito:username'];
+
+  const isEmailVerified = isInLocal
+    ? true
+    : event.requestContext.authorizer.claims.email_verified;
+
+  return { memberId, isEmailVerified };
+}
+**/
