68755c8edaefe543cc90114fac497bf793e84e7e
Mike Alicea <mfalicea58@gmail.com>
2022-05-05 15:19:13 -0400

    Using node_modules module with success

--- src/endpoints/api-get-wallet-by-user-id/get-wallet-by-user-id.ts
+++ src/endpoints/api-get-wallet-by-user-id/get-wallet-by-user-id.ts
@@ -1,9 +1,12 @@
 import type { APIGatewayProxyHandler } from 'aws-lambda';
 import { generateReturn } from '../api-util';
-// import log from 'lambda-log';
+// @ts-expect-error missing lambda-log types
+import log from 'lambda-log';
 
 export const handler: APIGatewayProxyHandler = async () => {
   try {
+    // eslint-disable-next-line
+    log.info('This is a test');
     return generateReturn(200, {
       wallet_key: 'asdf1234',
     });
