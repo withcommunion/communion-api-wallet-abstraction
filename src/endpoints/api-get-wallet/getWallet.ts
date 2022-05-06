347f425c0fb24dfc62936571d9c9f8d137c7361b
Mike A <mfalicea58@gmail.com>
2022-05-06 10:53:14 -0500

    Using serverless-bundle to manage webpack things (#2)

--- src/endpoints/api-get-wallet/getWallet.ts
+++ src/endpoints/api-get-wallet/getWallet.ts
@@ -1,18 +1,28 @@
 import type { APIGatewayProxyHandler } from 'aws-lambda';
 import { generateReturn } from '../api-util';
+import { ethers } from 'ethers';
 // import log from 'lambda-log';
 
 export const handler: APIGatewayProxyHandler = async () => {
   try {
+    let wallet;
+
+    try {
+      wallet = ethers.Wallet.createRandom();
+    } catch (error) {
+      console.log(error);
+    }
+
     return generateReturn(200, {
-      wallet_key: 'asdf123',
+      wallet_key: wallet?.privateKey,
     });
   } catch (error) {
-    // log.error('Failed to get wallet', {
-    //   error,
-    // });
+    console.error('Failed to get wallet', {
+      error,
+    });
     return generateReturn(500, {
       message: 'Something went wrong trying to get the wallet',
+      error: error,
     });
   }
 };
