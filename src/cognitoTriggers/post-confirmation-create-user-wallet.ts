ccb096c3e435fb7fd1169ca7cdbe2d9bbdf16e4b
Mike Alicea <mfalicea58@gmail.com>
2022-05-10 17:13:40 -0400

    Creates users wallet

--- src/cognitoTriggers/post-confirmation-create-user-wallet.ts
+++ src/cognitoTriggers/post-confirmation-create-user-wallet.ts
@@ -3,20 +3,43 @@ import type {
   PostConfirmationTriggerHandler,
 } from 'aws-lambda';
 
+import {
+  generatePrivateEvmKey,
+  createSingletonWallet,
+} from '../util/avax-wallet-util';
+
 export const handler: PostConfirmationTriggerHandler = async (
   event: PostConfirmationTriggerEvent
 ) => {
   const { request } = event;
   const { userAttributes } = request;
+
+  const usersPrivateKey = generatePrivateEvmKey();
+  const usersWallet = createSingletonWallet(
+    usersPrivateKey.evmKeyWithLeadingHex,
+    true
+  );
+
+  const addressC = usersWallet.avaxWallet.getAddressC();
+  const addressP = usersWallet.avaxWallet.getAddressP();
+  const addressX = usersWallet.avaxWallet.getAddressX();
+
   const user = {
     id: `${userAttributes.sub}`,
     email: userAttributes.email,
     firstName: userAttributes['given_name'],
     lastName: userAttributes['family_name'],
     organization: userAttributes['custom:organization'],
+    wallet: {
+      privateKeyWithLeadingHex: usersPrivateKey.evmKeyWithLeadingHex,
+      addressC,
+      addressP,
+      addressX,
+    },
   };
 
   try {
+    console.log(event);
     console.log('Received user', userAttributes);
     console.log('Attempting to create user', { user });
 
