234ecdd446e78bd574ce4f3dd71df874bff6fe33
Mike Alicea <mfalicea58@gmail.com>
2022-05-11 20:03:50 -0400

    Uses stage on table name

--- src/util/dynamo-util.ts
+++ src/util/dynamo-util.ts
@@ -10,7 +10,10 @@ export async function getWalletPrivateKeyByUserId() {
   //TODO Actually hit the DB.  This key is super temporary, it's okay to commit
   return '0x342d126ecba77ef9aa5fe9f28c1fb4443f7a5eb22315f7533a6b03a65d8bfd0a';
 }
-export const usersTable = 'userTable-dev';
+
+const stage = process.env.STAGE || 'dev';
+
+export const usersTable = `userTable-${stage}`;
 export const REGION = 'us-east-1';
 
 export function initDynamoClient(region: string = REGION) {
