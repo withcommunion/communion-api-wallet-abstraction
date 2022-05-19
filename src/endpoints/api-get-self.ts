6f6989ca33c86e4c989ce8434d3ce16c438fbaca
Mike Alicea <mfalicea58@gmail.com>
2022-05-19 11:37:46 -0500

    Changes get wallet by user id endpoint to getSelf

--- src/endpoints/api-get-self.ts
+++ src/endpoints/api-get-self.ts
@@ -1,32 +1,21 @@
-import type {
-  // APIGatewayAuthorizerHandler,
-  APIGatewayAuthorizerEvent,
-} from 'aws-lambda';
+import type { APIGatewayProxyEventV2WithJWTAuthorizer } from 'aws-lambda';
 import { generateReturn } from '../util/api-util';
 import { getUserById, initDynamoClient } from '../util/dynamo-util';
 
+const dynamoClient = initDynamoClient();
+
 export const handler = async (
-  event: APIGatewayAuthorizerEvent,
-  requestContext: string
+  event: APIGatewayProxyEventV2WithJWTAuthorizer
 ) => {
   try {
     console.log('incomingEvent', event);
-    console.log('incomingContext', requestContext);
-    // eslint-disable-next-line
-    console.log(
-      'incomingEventAuth',
-      // @ts-expect-error just for now
-      // eslint-disable-next-line
-      event.requestContext.authorizer
-    );
-    // @ts-expect-error just for now
-    // eslint-disable-next-line
+    console.log('incomingEventAuth', event.requestContext.authorizer);
+
     const username = event.requestContext.authorizer.jwt.claims
       .username as string;
+    // TODO: This is where and why we need to go by pure user key, not by urn
     const userUrn = `org-jacks-pizza-1:${username}`;
-    console.log('user', userUrn);
 
-    const dynamoClient = initDynamoClient();
     const user = await getUserById(dynamoClient, userUrn);
 
     return generateReturn(200, {
