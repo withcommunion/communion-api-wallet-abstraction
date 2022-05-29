870d27c3561f368b45f8349f4ca4fda0f5e8b44b
Mike A <mfalicea58@gmail.com>
2022-05-29 13:28:20 -0500

    Adds winston logging library (#8)
    * Adds winston
    
    * Adds logRetention:30days and LOG_LEVEL to env
    
    * Uses winston for logging
    
    * Uses winston
    
    * Local invoke improvements
    
    * log return value as info
    
    * Shorten request id for scanability

--- src/cognitoTriggers/post-confirmation-create-user-wallet.ts
+++ src/cognitoTriggers/post-confirmation-create-user-wallet.ts
@@ -1,20 +1,22 @@
-import type { PostConfirmationTriggerEvent } from 'aws-lambda';
+import type {
+  PostConfirmationTriggerEvent,
+  AuthResponseContext,
+} from 'aws-lambda';
+import { ethers } from 'ethers';
 
 import {
   generatePrivateEvmKey,
   createSingletonWallet,
 } from '../util/avax-wallet-util';
-
-import { ethers } from 'ethers';
-
 import { sendAvax } from '../util/avax-chain-util';
-
 import {
   initDynamoClient,
   insertUser,
   User,
   getUserById,
 } from '../util/dynamo-util';
+
+import logger from '../util/winston-logger-util';
 
 export const SEED_ACCOUNT_ID = '8f1e9bac-6969-4907-94f9-6187ec382976';
 export const BASE_AMOUNT_TO_SEED_USER = '0.01';
@@ -46,19 +48,45 @@ export async function seedFundsForUser(userCchainAddressToSeed: string) {
   return res;
 }
 
-export const handler = async (event: PostConfirmationTriggerEvent) => {
+export const handler = async (
+  event: PostConfirmationTriggerEvent,
+  context?: AuthResponseContext
+) => {
   try {
-    console.log('Incoming request:', event.request);
-
     const { request } = event;
     const { userAttributes } = request;
     const userId = userAttributes.sub;
+    const requestId = context?.awsRequestId
+      ? (context.awsRequestId as string)
+      : '';
+
+    logger.defaultMeta = {
+      _requestId: `${requestId?.substring(0, 8)}...${requestId?.substring(
+        30
+      )}}}`,
+      requestId,
+      userId,
+    };
+
+    logger.info('Incoming request event:', { values: { event } });
+    logger.verbose('Incoming request context:', { values: { context } });
 
     try {
+      logger.verbose('Checking if user exists in DB', { values: { userId } });
       const existingUser = await getUserById(dynamoClient, userId);
+      logger.verbose('User exists in DB', { values: { existingUser } });
 
-      if (existingUser && existingUser.wallet.privateKeyWithLeadingHex) {
-        console.log('Cognito fired twice, there is nothing to do here');
+      // TODO: Figure out how often this is happening
+      if (
+        existingUser &&
+        existingUser.wallet.privateKeyWithLeadingHex &&
+        process.env.NODE_ENV !== 'local'
+      ) {
+        logger.warn('Cognito fired twice, there is nothing to do here');
+        logger.info(
+          'User exists in DB and has a wallet.  Nothing to do, lets return',
+          { values: { event } }
+        );
         return event;
       }
     } catch (error) {
@@ -68,17 +96,25 @@ export const handler = async (event: PostConfirmationTriggerEvent) => {
     let usersPrivateKey;
     let usersWallet;
     try {
+      logger.verbose('Generating keys and wallets for user');
       usersPrivateKey = generatePrivateEvmKey();
       usersWallet = createSingletonWallet(
         usersPrivateKey.evmKeyWithLeadingHex,
         true
       );
+
+      logger.verbose('Generated keys and wallets for user', {
+        values: {
+          usersPrivateKey,
+          usersWallet,
+        },
+      });
     } catch (error) {
       /* Throw error.  We want to stop the lambda and prevent the user from verifying.
        * Something is very wrong here - this is essential for user function.
          TODO: Alert on this
      */
-      console.log('Error creating user wallet:', error);
+      logger.error('Error creating user wallet:', error);
       throw error;
     }
 
@@ -100,29 +136,29 @@ export const handler = async (event: PostConfirmationTriggerEvent) => {
     };
 
     try {
-      console.log('Attempting to create user', { user });
-      await insertUser(dynamoClient, user);
-      console.log('Created user');
+      logger.verbose('Attempting to create user', { values: { user } });
+      const respFromDb = await insertUser(dynamoClient, user);
+      logger.info('Created user', { values: { respFromDb } });
 
-      console.log('Attempting to seed user', {
+      logger.verbose('Attempting to seed user', {
         userAddress: user.wallet.addressC,
       });
       const sendAvax = await seedFundsForUser(user.wallet.addressC);
-      console.log('Seeded user', sendAvax);
+      logger.info('Seeded user', { values: { sendAvax } });
     } catch (error) {
       /* Throw error.  We want to stop the lambda and prevent the user from verifying.
      * Something is very wrong here - this is essential for user function.
        TODO: Alert on this
      */
-      console.error('Failed to create user', error);
+      logger.error('Failed to create and seed user', { values: { error } });
       throw error;
     }
 
     return event;
   } catch (error) {
-    console.error(
+    logger.error(
       'Error in cognito-triggers/post-confirmation-create-user-wallet.ts',
-      error
+      { values: { error } }
     );
     throw error;
   }
