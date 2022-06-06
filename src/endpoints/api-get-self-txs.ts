99fea85a310b6dc6bcf028c16baa0d29560c328e
Mike Alicea <mfalicea58@gmail.com>
2022-06-06 12:36:05 -0500

    Adds getSelfTxs - copy of get-self

---
+++ src/endpoints/api-get-self-txs.ts
@@ -0,0 +1,46 @@
+import type { APIGatewayProxyEventV2WithJWTAuthorizer } from 'aws-lambda';
+import { generateReturn } from '../util/api-util';
+import { getUserById, initDynamoClient } from '../util/dynamo-util';
+import logger from '../util/winston-logger-util';
+
+const dynamoClient = initDynamoClient();
+
+export const handler = async (
+  event: APIGatewayProxyEventV2WithJWTAuthorizer
+) => {
+  try {
+    const claims = event.requestContext.authorizer.jwt.claims;
+    // For some reason it can go through in two seperate ways
+    const userId =
+      (claims.username as string) || (claims['cognito:username'] as string);
+
+    logger.defaultMeta = {
+      _requestId: event.requestContext.requestId,
+      userId,
+    };
+
+    logger.info('incomingEvent', { values: { event } });
+    logger.verbose('incomingEventAuth', {
+      values: { authorizer: event.requestContext.authorizer },
+    });
+
+    logger.verbose('Fetching user', { values: { userId: userId } });
+    const user = await getUserById(dynamoClient, userId);
+    logger.verbose('Received user', { values: user });
+
+    const returnValue = generateReturn(200, {
+      ...user,
+    });
+    logger.info('Returning', { values: returnValue });
+
+    return returnValue;
+  } catch (error) {
+    logger.error('Failed to get wallet', {
+      values: error,
+    });
+    return generateReturn(500, {
+      message: 'Something went wrong trying to get the wallet',
+      error: error,
+    });
+  }
+};
